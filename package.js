0123a81776ccb66b37b60e748d41a641a55f3802
ian40k <groupanimators4@gmail.com>
2025-10-30 00:51:07 +0300

    Create package.js

---
+++ package.js
@@ -0,0 +1,20 @@
+{
+  "name": "whatsapp-media-bot",
+  "version": "1.0.0",
+  "description": "WhatsApp Bot with TikTok and Movie search",
+  "main": "api/bot.js",
+  "scripts": {
+    "dev": "node api/bot.js",
+    "start": "node api/bot.js"
+  },
+  "dependencies": {
+    "@whiskeysockets/baileys": "^6.4.0",
+    "express": "^4.18.0",
+    "axios": "^1.0.0",
+    "cheerio": "^1.0.0-rc.12",
+    "dotenv": "^16.0.0"
+  },
+  "engines": {
+    "node": ">=16.0.0"
+  }
+}
